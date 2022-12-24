8b8dfea1508cf2867c98d743a6649de48a6f33f1
FirasSayes99 <94109049+FirasSayes99@users.noreply.github.com>
2022-12-24 18:05:10 +0200

    create pivot table  for users and exams

---
+++ database/migrations/2022_12_24_154619_create_exam_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exam_user', function (Blueprint $table) {
+            
+            $table->float('right_answer');
+            $table->smallInteger('time_mins');
+            $table->enum('status',['open','closed']);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('exam_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exam_user');
+    }
+};
