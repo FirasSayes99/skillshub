7860d42367c6f245231177f0ae44008fdf5318a8
FirasSayes99 <94109049+FirasSayes99@users.noreply.github.com>
2022-12-24 21:37:38 +0200

    new page
    new page extends all layouts

---
+++ resources/views/admin/new.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.layouts.master')
+@section('title')
+new page
+@endsection
+
+
+@section('pagename')
+new page
+@endsection
+
+
+@section('main_page')
+new page
+@endsection
+
+@section('sub_page')
+new page
+@endsection
+
