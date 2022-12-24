50f9d9c0b867a579c2f44c8d20c73af3a1ad8bcb
FirasSayes99 <94109049+FirasSayes99@users.noreply.github.com>
2022-12-24 21:38:32 +0200

    updates web
    add dash board route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+});
+
+
+
+Route::get('/dashboard/new', function () {
+    return view('admin.new');
+});
