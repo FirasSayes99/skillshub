11db9f8e8f745b80756d82ac0e65ad4fc7df9523
firassayes <firassayes1999@gmail.com>
2022-12-25 15:17:44 +0200

    update web rout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,12 +21,20 @@ Route::get('/', function () {
 
 
 
+/* dashboard Controller */
+Route::get('/dashboard',[DashboardController::class,'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+
+/* dashboard Controller */
+/*
 Route::get('/dashboard', function () {
-    return view('admin.index');
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-
-
-Route::get('/dashboard/new', function () {
-    return view('admin.new');
-});
+*/
+require __DIR__.'/auth.php';
