9ddda9e2b8611f3110630c304ee69636a3b24838
FirasSayes99 <94109049+FirasSayes99@users.noreply.github.com>
2022-12-27 20:12:06 +0200

    Update dashboard.php

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -8,8 +8,11 @@ use App\Http\Controllers\Dashboard\CatsController;
 
 
 /* dashboard Controller */
-Route::get('/dashboard',[DashboardController::class,'index'])->middleware(['auth'])->name('dashboard');
+//Route::get('/dashboard',[DashboardController::class,'index'])->middleware(['auth'])->name('dashboard');
 
+Route::get('dashboard/cats/archive',[CatsController::class,'archive'])->name('cats.archive');
+Route::get('dashboard/cats/restore/{id}',[CatsController::class,'restore'])->name('cats.restore');
+Route::get('dashboard/cats/force/{id}',[CatsController::class,'force'])->name('cats.force');
 
 Route::resource('/dashboard/cats',CatsController::class);
 
