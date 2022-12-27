084196126c5ad07dbf82b062e51f4a69f65b877b
FirasSayes99 <94109049+FirasSayes99@users.noreply.github.com>
2022-12-27 20:03:35 +0200

    Update Skills.php
    add relationship

--- app/Models/Skills.php
+++ app/Models/Skills.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Skills extends Model
 {
     use HasFactory;
+
+    public function Cats(){
+        return $this->belongsTo(Cats::class);
+    }
+
+    public function exams(){
+        return $this->hasMany(Exam::class);
+    }
 }
