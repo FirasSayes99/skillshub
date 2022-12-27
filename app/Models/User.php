285aa039923b3cd65460e427e016bb661601eea4
FirasSayes99 <94109049+FirasSayes99@users.noreply.github.com>
2022-12-27 20:03:57 +0200

    Update User.php
    add relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'password',
+        'role_id'
     ];
 
     /**
@@ -42,4 +43,12 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Role(){
+        return $this->belongsTo(Role::class);
+    }
+
+    public function exams(){
+        return $this->belongsToMany(Exam::class);
+    }
 }
