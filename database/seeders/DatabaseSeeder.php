d52a5f7a45c26b84b72a66fdf34b1a667ed7fddd
FirasSayes99 <94109049+FirasSayes99@users.noreply.github.com>
2023-01-01 08:52:12 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
+        $this->call(CatSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
