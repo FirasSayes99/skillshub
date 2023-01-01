46ff2fdccbfb76bc0fe370769ddfebe8e4b2a9eb
FirasSayes99 <94109049+FirasSayes99@users.noreply.github.com>
2023-01-01 08:56:09 +0200

    Update master.blade.php
    remove contact section

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -14,32 +14,7 @@
 
 
     <!-- Contact CTA -->
-    <div id="contact-cta" class="section">
-
-        <!-- Backgound Image -->
-        <div class="bg-image bg-parallax overlay" style="background-image:url({{asset('web/assets/img/cta.jpg')}})"></div>
-        <!-- Backgound Image -->
-
-        <!-- container -->
-        <div class="container">
-
-            <!-- row -->
-            <div class="row">
-
-                <div class="col-md-8 col-md-offset-2 text-center">
-                    <h2 class="white-text">Contact Us</h2>
-                    <p class="lead white-text">Libris vivendo eloquentiam ex ius, nec id splendide abhorreant.</p>
-                    <a class="main-button icon-button" href="contact.html">Contact Us Now</a>
-                </div>
-
-            </div>
-            <!-- /row -->
-
-        </div>
-        <!-- /container -->
-
-    </div>
-    <!-- /Contact CTA -->
+    
 
     <!-- Footer -->
 
