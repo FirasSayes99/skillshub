6ef76e2e1da09ee1607ac7f364255961241af9ca
FirasSayes99 <94109049+FirasSayes99@users.noreply.github.com>
2023-01-01 08:47:01 +0200

    Update CatsController.php
    add data array include all information

--- app/Http/Controllers/Web/CatsController.php
+++ app/Http/Controllers/Web/CatsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Models\Cats;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 
 
@@ -16,9 +17,14 @@ class CatsController extends Controller
     }
 
     public function show($id){
-        $cat = Cats::findOrFail($id);
+        //$cat =Cats::findOrFail($id);
+        //$cats = Cats::select('id','name')->get();//// طريقة اضافة ال داتا بال كومباكت compact
+        $data['cat']=Cats::findOrFail($id);
+        $data['cats']=Cats::select('id','name')->get();
+        $data['skills']=$data['cat']->skills;
+        
 
-        return view('web.cat.show',compact('cat'));
+        return view('web.cat.show')->with($data);
 
     }
 }
