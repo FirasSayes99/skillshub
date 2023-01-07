9c0d84fbcd6cae70dc82cf42b6b485d5c929ab6d
FirasSayes99 <94109049+FirasSayes99@users.noreply.github.com>
2023-01-07 19:06:20 +0200

    Update web.php
    add skills & exams routs

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Web\CatsController;
+use App\Http\Controllers\Web\ExamController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Web\SkilsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,6 +33,11 @@ Route::get('cat/show/{id}',[catsController::class,'show'])->name('cat.show');
 Route::get('/dashboard',[DashboardController::class,'index'])->middleware(['auth'])->name('dashboard');
 Route::get('/contact',[ContactController::class,'index'])->name('contact.create');
 Route::POST('/contact/message',[ContactController::class,'send'])->name('contact.store');
+
+Route::get('/skills/show/{id}',[SkilsController::class,'show'])->name('skills.show');
+
+Route::get('/exams/show/{id}',[ExamController::class,'show'])->name('exams.show');
+Route::get('/exams/questions/{id}',[ExamController::class,'questions'])->name('exams.questions');
 
 
 /* dashboard Controller */
