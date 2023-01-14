b0e5e9d343c58b6f09eb7947be0259d6eeba8e92
FirasSayes99 <94109049+FirasSayes99@users.noreply.github.com>
2023-01-14 21:02:45 +0200

    add Fortify features
    add Fortify features

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -35,7 +35,22 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::registerView(function () {
             return view('auth.register');
         });
+
+
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
         
+        Fortify::verifyEmailView(function () {
+            return view('auth.verify-email');
+        });
+
+        Fortify::requestPasswordResetLinkView(function () {
+            return view('auth.forgot-password');
+        });
+        Fortify::resetPasswordView(function ($request) {
+            return view('auth.reset-password', ['request' => $request]);
+        });
 
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
